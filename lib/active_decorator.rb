41a2d8d3a82c65153bbc5050f8eabdbe8981f8df
lulalala <mark@goodlife.tw>
2015-11-05 21:46:13 +0800

    Remove unused decoration class and monkey patch

--- lib/active_decorator.rb
+++ lib/active_decorator.rb
@@ -1,5 +1,4 @@
 require 'active_decorator/version'
-require 'active_decorator/decorator'
 require 'active_decorator/base_decorator'
 require 'active_decorator/railtie'
 require 'active_decorator/config'
